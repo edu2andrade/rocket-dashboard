fd79c4ce5e085b190d445143ab98851d1b5d2dec
Eduardo Andrade <edu2andrade@gmail.com>
2022-07-18 15:50:17 +0100

    Start making the app responsive, Header and Sidebar done, SidebarDrawerContext created

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Box, Flex, Text, Avatar } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Eduardo Andrade</Text>
-        <Text color="gray.300" fontSize="small">
-          eduardo@andradept.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Eduardo Andrade</Text>
+          <Text color="gray.300" fontSize="small">
+            eduardo@andradept.com
+          </Text>
+        </Box>
+      )}
       <Avatar
         size="md"
         name="Eduardo Andrade"
